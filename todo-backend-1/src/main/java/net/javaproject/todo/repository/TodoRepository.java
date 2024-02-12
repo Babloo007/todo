4b43e8480b52b12ce04096ed6e9f7a8dbc1d0abe
Babloo007 <msvreddy77@gmail.com>
2024-02-12 16:07:03 +0530

    merging

--- todo-backend-1/src/main/java/net/javaproject/todo/repository/TodoRepository.java
+++
@@ -1,7 +0,0 @@
-package net.javaproject.todo.repository;
-
-import net.javaproject.todo.entity.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-}
